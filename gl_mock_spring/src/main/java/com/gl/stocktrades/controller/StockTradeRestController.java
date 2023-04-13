675d56f6395053708b64ebb2e45cf21a5ebf2f56
Anjali Chaudhary <anjalic1796@gmail.com>
2023-04-14 00:26:42 +0530

    minor change

--- gl_mock_spring/src/main/java/com/gl/stocktrades/controller/StockTradeRestController.java
+++ gl_mock_spring/src/main/java/com/gl/stocktrades/controller/StockTradeRestController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/trades")
 public class StockTradeRestController {
+  
 
   @Autowired
   StockTradeService stockTradeService;
